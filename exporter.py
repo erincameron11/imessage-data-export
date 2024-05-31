76a9a734870596e7c150cef157a8ca9af34122be
Erin Cameron <erincameron@lauras-mbp.home>
2024-05-31 14:58:46 -0400

    initial commit

---
+++ exporter.py
@@ -0,0 +1,10 @@
+# Main iMessage Data Export file for backing up iMessage chats, videos, and images
+
+class Exporter:
+    # Object Initializer
+    def __init__(self):
+        # Do something here
+        db = "file goes here"
+
+
+    
